a780b77fb9f1552bfba82247ca0d4d7bc4d533fc
Lucas-sa <lucas151@hotmail.com>
2022-05-16 19:32:56 -0300

    03

--- pages/finalizar.js
+++ pages/finalizar.js
@@ -5,7 +5,7 @@ export default function Finaliar() {
       <div className="col-10 col-md-6 m-auto mt-5 color py-5 text-white text-center h5 rounded shadow animate__animated animate__flipInX">
         <div className="mb-2 h1 pt-5 text-uppercase"><strong>Parabéns</strong></div>
         <div className="pb-5"><strong><em>Sua compra finalizada com sucesso</em></strong></div>
-        <a href='/' className="col-4 btn btn-lg btn-finalizar text-white shadow"><strong>Voltar</strong></a>
+        <a href='./' className="col-4 btn btn-lg btn-finalizar text-white shadow"><strong>Voltar</strong></a>
       </div>
     </div>
   )
